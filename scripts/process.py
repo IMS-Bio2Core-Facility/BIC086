e6f72ea292bc7d8f6fd87828cfe17c3e8841dd68
rbpatt2019 <rb.patterson.cross@gmail.com>
2021-05-28 13:36:06 +0100

    Add if/main protection to scripts

--- scripts/process.py
+++ scripts/process.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 """Process GTEx json data to xlsx."""
-import pandas as pd
+if __name__ == "__main__":
+    import pandas as pd
+    from logs.get_logger import get_logger
+    from multithreading.process import Pipeline
 
-from logs.get_logger import get_logger
-from multithreading.process import Pipeline
+    INS = snakemake.input  # noqa: F821
+    LOGS = snakemake.log[0]  # noqa: F821
+    OUTS = snakemake.output  # noqa: F821
+    THREADS = snakemake.threads  # noqa: F821
 
-INS = snakemake.input  # noqa: F821
-LOGS = snakemake.log[0]  # noqa: F821
-OUTS = snakemake.output  # noqa: F821
-THREADS = snakemake.threads  # noqa: F821
+    logger = get_logger(__name__, LOGS)
 
-logger = get_logger(__name__, LOGS)
-
-with pd.ExcelWriter(OUTS["data"], engine="openpyxl") as writer:
-    pl = Pipeline(files=INS["data"], writer=writer, maxworkers=THREADS)
-    pl.run()
+    with pd.ExcelWriter(OUTS["data"], engine="openpyxl") as writer:
+        pl = Pipeline(files=INS["data"], writer=writer, maxworkers=THREADS)
+        pl.run()
