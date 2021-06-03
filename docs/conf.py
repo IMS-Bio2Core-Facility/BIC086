443facc6ff18995fd39ee0f7cabb8840a073f02f
rbpatt2019 <rb.patterson.cross@gmail.com>
2021-06-03 10:46:31 +0100

    Rename repository

--- docs/conf.py
+++ docs/conf.py
@@ -1,21 +1,2 @@
 # -*- coding: utf-8 -*-
-"""Sphinx configuration."""
-import os
-import sys
-
-import sphinx_rtd_theme
-
-sys.path.insert(0, os.path.abspath("../"))
-sys.path.insert(0, os.path.abspath("../tests/"))
-sys.path.insert(0, os.path.abspath("../scripts/"))
-
-project = "BIC086-Sophie-Austin"
-author = "Ryan B Patterson-Cross"
-copyright = "2021, IMS-MRL Bioinformatics and Biostatistic Core"
-extensions = ["sphinx_rtd_theme", "sphinx.ext.autodoc", "sphinx.ext.napoleon"]
-
-napoleon_google_docstrings = False
-napoleon_numpy_docstrings = True
-napoleon_use_param = False
-
-html_theme = "sphinx_rtd_theme"
+project = "BIC086"
